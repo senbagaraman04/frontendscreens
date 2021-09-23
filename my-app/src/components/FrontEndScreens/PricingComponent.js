32b7c5bd0d791e4c90e24c89183e825ae2ea95c0
Senbagaraman <senbagaraman.manoharan@siemens.com>
2021-09-23 17:35:56 +0530

    feat: cards sample

--- my-app/src/components/FrontEndScreens/PricingComponent.js
+++ my-app/src/components/FrontEndScreens/PricingComponent.js
@@ -1,12 +1,84 @@
 import React, { Component } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card, CardGroup, Container } from "react-bootstrap";
+
 
 export default class PricingComponents extends Component {
     render() {
         return (
-            <div style={{ display: 'flex', justifyContent: 'center', padding: 30 }}>
-                <div><h2>Pricing Component Under Construction</h2>
-                </div>
-            </div>
+            <Container>
+            <CardGroup>
+<Card1/>
+<Card2/>
+<Card3/>      
+        
+          </CardGroup>
+           </Container>
+        );
+    }
+}
+
+
+class Card1 extends Component {
+    render(){
+        return(
+            <Card>              
+            <Card.Body>
+              <Card.Title>Basic</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+         
+        
+        );
+    }
+}
+
+class Card2 extends Component {
+    render(){
+        return(
+            <Card>              
+            <Card.Body>
+              <Card.Title>Professional</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+         
+        
+        );
+    }
+}
+
+
+class Card3 extends Component {
+    render(){
+        return(
+            <Card>              
+            <Card.Body>
+              <Card.Title>Master</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+         
+        
         );
     }
 }
