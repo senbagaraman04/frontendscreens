791bf2bf97df1ae90c4a3978a649bef4ed903c1f
Senbagaraman <senbagaraman.manoharan@siemens.com>
2021-09-23 18:19:52 +0530

    fix: cards pricing

--- my-app/src/components/FrontEndScreens/PricingComponent.js
+++ my-app/src/components/FrontEndScreens/PricingComponent.js
@@ -1,86 +1,144 @@
+/**
+ * https://www.frontendmentor.io/challenges/pricing-component-with-toggle-8vPwRMIC/
+ * Author: @senbagaraman04
+ */
+
 import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Card, CardGroup, Container } from "react-bootstrap";
+import { Button, Card, CardGroup, Container } from "react-bootstrap";
+import { Divider, List, ListItem, ListItemText } from '@mui/material';
 
 
 export default class PricingComponents extends Component {
     render() {
         return (
             <Container>
-            <CardGroup>
-<Card1/>
-<Card2/>
-<Card3/>      
-        
-          </CardGroup>
-           </Container>
+                <CardGroup>
+                    <Card1 />
+                    <Card2 />
+                    <Card3 />
+
+                </CardGroup>
+            </Container>
         );
     }
 }
 
 
 class Card1 extends Component {
-    render(){
-        return(
-            <Card>              
-            <Card.Body>
-              <Card.Title>Basic</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-         
-        
+    render() {
+        return (
+            <Card>
+                <Card.Body>
+                    <Card.Title>Basic</Card.Title>
+                    <Card.Text>
+                        <List>
+                            <ListItem disablePadding>
+                                <ListItemText primary="$199.99" />
+                            </ListItem>
+                            <Divider />
+                            <ListItem disablePadding>
+                                <ListItemText primary="500 GB storage" />
+                            </ListItem>
+                            <Divider />
+
+                            <ListItem disablePadding>
+                                <ListItemText primary="2 Users allowed" />
+                            </ListItem>
+                            <Divider />
+
+                            <ListItem disablePadding>
+                                <ListItemText primary="Send up to 3 GB" />
+                            </ListItem>
+                            <Divider />
+
+                            <Button variant="outline-info" Color="Success">Learn More</Button>
+                        </List>
+                    </Card.Text>
+                </Card.Body>
+
+            </Card>
+
+
         );
     }
 }
 
 class Card2 extends Component {
-    render(){
-        return(
-            <Card>              
-            <Card.Body>
-              <Card.Title>Professional</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-         
-        
+    render() {
+        return (
+            <Card>
+                <Card.Body>
+                    <Card.Title>Professional</Card.Title>
+                    <Card.Text>
+                        <List>
+                            <ListItem disablePadding>
+                                <ListItemText primary="$249.99" />
+                            </ListItem>
+                            <Divider />
+                            <ListItem disablePadding>
+                                <ListItemText primary="1TB Storage" />
+                            </ListItem>
+                            <Divider />
+
+                            <ListItem disablePadding>
+                                <ListItemText primary="5 Users allowed" />
+                            </ListItem>
+                            <Divider />
+
+                            <ListItem disablePadding>
+                                <ListItemText primary="Send up to 10 GB" />
+                            </ListItem>
+                            <Divider />
+
+                            <Button variant="outline-info" Color="Success">Learn More</Button>
+                        </List>
+                    </Card.Text>
+                </Card.Body>
+
+            </Card>
+
+
         );
     }
 }
 
 
 class Card3 extends Component {
-    render(){
-        return(
-            <Card>              
-            <Card.Body>
-              <Card.Title>Master</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-         
-        
+    render() {
+        return (
+            <Card>
+                <Card.Body>
+                    <Card.Title>Master</Card.Title>
+                    <Card.Text>
+                        <List>
+                            <ListItem disablePadding>
+                                <ListItemText primary="$399.99" />
+                            </ListItem>
+                            <Divider />
+                            <ListItem disablePadding>
+                                <ListItemText primary="500 GB storage" />
+                            </ListItem>
+                            <Divider />
+
+                            <ListItem disablePadding>
+                                <ListItemText primary="10 Users allowed" />
+                            </ListItem>
+                            <Divider />
+
+                            <ListItem disablePadding>
+                                <ListItemText primary="Send up to 20 GB" />
+                            </ListItem>
+                            <Divider />
+
+                            <Button variant="outline-info" Color="Success">Learn More</Button>
+                        </List>
+                    </Card.Text>
+                </Card.Body>
+
+            </Card>
+
+
         );
     }
 }
-
- 
