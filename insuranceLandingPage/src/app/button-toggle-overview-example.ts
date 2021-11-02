1a03a2c1bb56e745597d2eaafeefafb572ef3036
Senbagaraman <senbagaraman.manoharan@siemens.com>
2021-11-02 17:40:16 +0530

    fix: Angular Stable Page

---
+++ insuranceLandingPage/src/app/button-toggle-overview-example.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic button-toggles
+ */
+@Component({
+  selector: 'button-toggle-overview-example',
+  templateUrl: 'button-toggle-overview-example.html',
+})
+export class ButtonToggleOverviewExample {}
+
+
+/**  Copyright 2021 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
