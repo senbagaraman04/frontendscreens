dcafef6ea789fceee09607f321d650d8209d6e83
Senbagaraman <senbagaraman.manoharan@siemens.com>
2021-11-02 17:10:11 +0530

    fix: init

--- insuranceLandingPage/src/app/app.module.ts
+++ insuranceLandingPage/src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatButtonModule, MatSidenavModule, MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
